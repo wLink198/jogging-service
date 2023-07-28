97274dc7760713272aa44a53581c168e4b641258
Linhbq <linhbq.it@tripi.vn>
2023-07-28 10:17:09 +0700

    init

---
+++ api/src/main/java/com/svc/jogging/api/JoggingApplication.java
@@ -0,0 +1,12 @@
+package com.svc.jogging.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoggingApplication.class, args);
+	}
+}
---
+++ api/src/test/java/com/svc/jogging/JoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svc.jogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
