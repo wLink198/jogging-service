18e5796ee4044bda58cbb4878c9f3727fc8b451f
Linhbq <linhbq.it@tripi.vn>
2023-07-30 09:54:11 +0700

    untested version of apis

--- api/src/main/java/com/svc/jogging/api/JoggingApplication.java
+++ api/src/main/java/com/svc/jogging/api/JoggingApplication.java
@@ -2,7 +2,11 @@ package com.svc.jogging.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = { "com.svc.jogging" })
+@EnableCaching
 @SpringBootApplication
 public class JoggingApplication {
 
